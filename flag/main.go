195fac02f0ba17217cd20cf1ff122edea95d7cc2
mathieu001 <scj02@163.com>
2021-02-11 17:47:19 +0800

    final commit of flag

--- flag/main.go
+++ flag/main.go
@@ -5,23 +5,23 @@ import (
 	"log"
 )
 
-func main(){
+func main() {
 	var name string
 
 	flag.Parse()
-	args:=flag.Args()
-	if len(args)<=0{
+	args := flag.Args()
+	if len(args) <= 0 {
 		return
 	}
 	switch args[0] {
 	case "go":
-		goCmd:=flag.NewFlagSet("go",flag.ExitOnError)
-		goCmd.StringVar(&name,"name","go语言","帮助信息")
+		goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+		goCmd.StringVar(&name, "name", "go语言", "帮助信息")
 		goCmd.Parse(args[1:])
 	case "php":
-		goCmd:=flag.NewFlagSet("php",flag.ExitOnError)
-		goCmd.StringVar(&name,"name","php语言","帮助信息")
+		goCmd := flag.NewFlagSet("php", flag.ExitOnError)
+		goCmd.StringVar(&name, "name", "php语言", "帮助信息")
 		goCmd.Parse(args[1:])
 	}
-	log.Printf("name= %s",name)
+	log.Printf("name= %s", name)
 }
